50f39f4972c183b96e50dcd93a602bfe9106ec7f
an2434 <jinhyung2434@naver.com>
2022-08-08 22:13:25 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
-import { Container, Nav, Navbar, NavDropdown, Col, Row } from 'react-bootstrap';
-import { data } from './data';
-import Goods from './GoodsComponent';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { Container, Nav, Navbar, NavDropdown, Col, Row } from "react-bootstrap";
+import { data } from "./data";
+import Goods from "./GoodsComponent";
 
 function App() {
-
-  
-
   return (
     <div className="App">
       <div className="header">
-        <div className='title'></div>
+        <div className="title"></div>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand href="#home" >NotShop</Navbar.Brand>
+            <Navbar.Brand href="#home">NotShop</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
@@ -28,7 +23,9 @@ function App() {
                   <NavDropdown.Item href="#action/3.2">
                     Another action
                   </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="#action/3.4">
                     Separated link
@@ -44,31 +41,16 @@ function App() {
             </Navbar.Collapse>
           </Container>
         </Navbar>
-
       </div>
       <div className="body">
-        <div className='main-bg'></div>
+        <div className="main-bg"></div>
 
         <div className="product-display">
-
           <Container>
-
             <Row>
-              <Col sm>
-                <img className='Goods' src="https://codingapple1.github.io/shop/shoes1.jpg"></img>
-               <Goods></Goods>
-              </Col>
-              <Col sm>
-                <img className='Goods' src="https://codingapple1.github.io/shop/shoes2.jpg"></img>
-                <Goods></Goods>
-              </Col>
-              <Col sm>
-                <img className='Goods' src="https://codingapple1.github.io/shop/shoes3.jpg"></img>
-                <Goods></Goods>
-              </Col>
+              <Goods />
             </Row>
           </Container>
-
         </div>
       </div>
     </div>
--- src/GoodsComponent.js
+++ src/GoodsComponent.js
@@ -1,27 +1,26 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
-import { Container, Nav, Navbar, NavDropdown, Col, Row } from 'react-bootstrap';
-import { data } from './data';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { Container, Nav, Navbar, NavDropdown, Col, Row } from "react-bootstrap";
+import { data } from "./data";
 
 const Goods = () => {
-    const a1 = [0, 1, 2];
-    return (
-
-        a1.map((i) => {
-            return (
-                <Col sm>
-
-                    {data[i].id}
-                    <h4 >{data[i].title}</h4>
-                    <p>{data[i].content}</p>
-                    <p>{data[i].price}</p>
-                </Col>
-            )
-        }
-        )
-
-    );
-}
+  const a1 = [0, 1, 2];
+  return (
+    <>
+      {a1.map((i) => (
+        <Col key={i}>
+          <img
+            className="Goods"
+            src={`https://codingapple1.github.io/shop/shoes${i + 1}.jpg`}
+          ></img>
+          <h4>{data[i].title}</h4>
+          <p>{data[i].content}</p>
+          <p>{data[i].price}</p>
+        </Col>
+      ))}
+    </>
+  );
+};
 
 export default Goods;
